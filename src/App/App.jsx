284fb1d53e58477c30bb0341931423d3aca16368
UsernameAK <sitemakers.3dn.ru@gmail.com>
2017-09-18 22:56:55 +0300

    codemirror

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import './App.css';
+import CodeMirror from 'react-code-mirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Sidebar />
+        <CodeMirror theme="monokai" style={{
+          height: '100vh',
+          width: '100vw'
+        }} lineNumbers={true}/>
       </div>
     );
   }
