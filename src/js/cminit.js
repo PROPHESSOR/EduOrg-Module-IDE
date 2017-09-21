b127161f630266acbff19f715b535e0156cd0b57
PROPHESSOR <xenteze@gmail.com>
2017-09-21 18:07:48 +0300

    codemirror set

---
+++ src/js/cminit.js
@@ -0,0 +1,24 @@
+let editor = CodeMirror.fromTextArea($("#code")[0], {
+    lineNumbers: true,
+    mode: "text/javascript",
+    matchBrackets: true,
+    autoCloseBrackets: true,
+    extraKeys: {
+        "Ctrl-Tab": "autocomplete",
+        "Ctrl-/": "toggleComment"
+    },
+    continueComments: "Enter",
+});
+editor.setOption("theme", "monokai");
+editor.setSize("100%", "100%");
+editor.on("keyup", function (e, s) {
+    $rampage = s;
+    if (s.type == "keyup" && !e.state.completionActive && s.keyCode != 13 && s.keyCode !=
+        27 && s.keyCode != 32 && s.keyCode != 8 && /^[A-Zа-яА-Яa-z$_]*$/.test(s.key) && s.key
+        .length == 1 && !s.ctrlKey && !s.altKey) {
+        CodeMirror.showHint(e, null, {
+            completeSingle: false
+        });
+        // CodeMirror.commands.autocomplete(e, null, {completeSingle: false});
+    }
+});
